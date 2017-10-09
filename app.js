1b126dd4046763d8eb4753e616df5003ce3b1a4f
ramesh901 <rameshinbox901@gmail.com>
2017-10-09 16:59:25 +0530

    Add task to local storage

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
+/* PENDING ITEMS
+COMPLETED TASKS HAS TO BE WITH 'CHECK MARK' AND STRIKETHRU
+DELETED ITEM HAS TO BE REMOVED FROM LOCAL STORAGE
+DO SOME NICE FRONT END
+ */
+
+function taskFromStorage () {
+  var localTask = localStorage.getItem('task')
+  if (localTask) {
+    JSON.parse(localTask).forEach((item) => {
+      console.log('items are', item)
+      var ptag = document.createElement('p')
+      ptag.textContent = item
+      addedItem.appendChild(ptag)
+    })
+  }
+}
+
 var list = document.querySelectorAll('#addedItem')
 var addedItem = document.querySelector('#addedItem')
 var add = document.forms['todo']
-//var add = document.querySelector('#add')
+taskFromStorage()
+var valueStorage = []
+// var add = document.querySelector('#add')
 add.addEventListener('submit', addTask)
 function addTask (e) {
-    e.preventDefault()
-    var value = add.querySelector('input[type="text"]').value
-    var ptag = document.createElement('p')
-    ptag.textContent = value
-    addedItem.appendChild(ptag)
+  e.preventDefault()
+  var value = add.querySelector('input[type="text"]').value
 
+  valueStorage.push(value)
+  console.log(valueStorage)
+  localStorage.setItem('task', JSON.stringify(valueStorage))
+  var ptag = document.createElement('p')
+  ptag.textContent = value
+  addedItem.appendChild(ptag)
 }
 
-console.log(list)
+// console.log(list)
 Array.from(list).forEach((item) => {
-    item.addEventListener('click', (e) => {
-        if(e.target.tagName = 'p'){
-            const li = e.target;
-            li.parentNode.removeChild(li);
-            
-        }
-    
-    })
-
+  item.addEventListener('click', (e) => {
+    if (e.target.tagName === 'p') {
+      var li = e.target
+      li.parentNode.removeChild(li)
+    }
+  })
 })
